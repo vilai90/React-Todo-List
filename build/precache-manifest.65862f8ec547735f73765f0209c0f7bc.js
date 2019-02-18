ff222ad57ed19ca87401acad0be01cc5a3bafac6
Vincent Lai <vilai90@gmail.com>
2019-02-18 15:05:53 -0800

    Removal of build from gitignore

---
+++ build/precache-manifest.65862f8ec547735f73765f0209c0f7bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "287d250a2019b68bc214",
+    "url": "/static/css/main.f78cd134.chunk.css"
+  },
+  {
+    "revision": "287d250a2019b68bc214",
+    "url": "/static/js/main.287d250a.chunk.js"
+  },
+  {
+    "revision": "204459de83d719cb7dca",
+    "url": "/static/css/1.1eb9dc15.chunk.css"
+  },
+  {
+    "revision": "204459de83d719cb7dca",
+    "url": "/static/js/1.204459de.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5199204ff3bb3b20707d311543f4f3d6",
+    "url": "/index.html"
+  }
+];
